3eefd48394305282e40b3eedd49726db3be32904
Shu11336 <moazshouman@gmail.com>
2024-10-06 12:52:19 +0300

    basic data types

--- python data types.py
+++ python data types.py
@@ -3,13 +3,18 @@ from xmlrpc.client import boolean
 
 #string
 print("hello")
+print(type("hello"))
+
 
 #int
 print (1234)
+print (type(1234))
 
 #float
 print (321.123)
+print (type(321.123))
 
 # boolean
 print(True)
+print(type(True))
 
