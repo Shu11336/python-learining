200e14911fdbea10e5478bae841c3c8bf7032cf2
Shu11336 <moazshouman@gmail.com>
2024-10-06 13:55:06 +0300

    mathematical ops in python and f string

---
+++ if else.py
@@ -0,0 +1,7 @@
+# height test for entering a roller coaster
+height = int(input("Enter your height"))
+
+if height >= 130 :
+    print("you can enter ")
+else:
+    print("your height is not enough to enter")
