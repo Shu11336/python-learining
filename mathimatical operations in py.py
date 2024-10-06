9f94b8482e935aeef1165016a77e64a371372fbf
Shu11336 <moazshouman@gmail.com>
2024-10-06 13:06:57 +0300

    mathematical ops in python

---
+++ mathimatical operations in py.py
@@ -0,0 +1,18 @@
+# +
+print (1234 +123)
+
+# -
+print (34 - 4)
+
+# multiply
+print (12 * 12)
+
+# division
+print ( 12 / 4)
+
+#division (no float)
+print (5 / 3)
+print (5 // 3)
+
+#squared
+print (2 ** 3)
