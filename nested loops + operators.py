9a8350dc4e79f56292a8d7aebede2286ae778507
Shu11336 <moazshouman@gmail.com>
2024-10-06 14:05:04 +0300

    mathematical ops in python and f string

---
+++ nested loops + operators.py
@@ -0,0 +1,18 @@
+#elif
+# DETERMINE what are your grades (A.B.C.D.E)
+
+grade = int(input("enter your grade"))
+
+if grade >= 90:
+    print("your grade is A")
+elif grade >= 80:
+       print("your grade is B")
+elif grade >= 70:
+       print("your grade is C")
+elif grade >= 60:
+       print("your grade is D")
+elif grade >= 50:
+       print("your grade is E")
+else:
+    print("your grade is F")
+
