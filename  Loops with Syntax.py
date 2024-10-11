e14edff4bb26fc4a113ba621e8d868d3d73ac66a
Shu11336 <moazshouman@gmail.com>
2024-10-11 11:39:04 +0300

    for in py

---
+++  Loops with Syntax.py
@@ -0,0 +1,18 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+  print(fruit)
+
+colors = ("red", "green", "blue")
+
+for color in colors:
+  print(color)
+
+  message = "Hello, world!"
+
+for character in message:
+      print(character)
+
+for number in range(1, 6):
+    print(number)
+
